0ddd8239a5594740b461d9b12fca160d7b9b5bf8
Anup Kumar Mondal <bca.anup@gmail.com>
2024-02-13 22:05:26 +0530

    TRAINING2024-Feb06-00

---
+++ YEAR2024/TRAINING2024-Feb06-00/day2-arguments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hey, Bash!"
+
+echo "Hey, Bash! $0 $1 $2"
+
+<< comment
+[anup@automation-and-continuous-delivery YEAR2024]$ chmod 754 day2-arguments.sh 
+[anup@automation-and-continuous-delivery YEAR2024]$ ./day2-arguments.sh
+[anup@automation-and-continuous-delivery YEAR2024]$ ./day2-arguments.sh Hello Whatsup
+comment
+
